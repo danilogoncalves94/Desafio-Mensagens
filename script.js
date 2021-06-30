a3bd0f67d4a5f0d32f6484028afd73eafed2378a
danilogoncalves94 <82965321+danilogoncalves94@users.noreply.github.com>
2021-06-30 15:16:23 -0300

    updates

---
+++ script.js
@@ -0,0 +1,9 @@
+function clique1() {
+    window.alert('Você clicou no 1° botão!')
+}
+function clique2() {
+    window.alert('Você clicou no 2° botão!')
+}
+function clique3() {
+    window.alert('Você clicou no 3° botão!')
+}
